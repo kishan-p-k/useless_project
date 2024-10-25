1fd9ffa5b9027930600f53c33962106a8b4f4e50
KISHAN P K <kishankishorkumar.p.k@gmail.com>
2024-10-25 22:47:48 +0530

    jn

--- lib/home.dart
+++ lib/home.dart
@@ -25,7 +25,6 @@ class HomePage extends StatelessWidget {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => LoginPage()),
-                  MaterialPageRoute(builder: (context) => LoginPage()),
                 );
               }
             },
